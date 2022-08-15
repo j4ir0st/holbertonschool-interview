0758159e59debfb84e3b825cf66791700bc83fdd
j4ir0st <jairocast2@gmail.com>
2022-08-15 15:12:45 -0500

    changes

---
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * _binary_tree_delete - Deallocate a binary tree
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}

6befbba82ddf6f08557283eb0056e86ee2393137
j4ir0st <jairocast2@gmail.com>
2022-08-17 10:31:49 -0500

    Fixing betty style

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * _binary_tree_delete - Deallocate a binary tree
+ * binary_tree_node - Deallocate a binary tree
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
+ * Return: return the new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
